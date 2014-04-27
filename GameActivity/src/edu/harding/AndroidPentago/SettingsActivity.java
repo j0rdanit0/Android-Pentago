d7b2d16fe5525416b48fb5b033105f1d81ce7f95
T-RICH-92 <trichar2@harding.edu>
2014-04-26 19:50:54 -0500

    Settings should work now

--- GameActivity/src/edu/harding/AndroidPentago/SettingsActivity.java
+++ GameActivity/src/edu/harding/AndroidPentago/SettingsActivity.java
@@ -1,46 +1,44 @@
 package edu.harding.AndroidPentago;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.SeekBar;
+
 import edu.harding.androidtictactoe.R;
 
-public class SettingsActivity extends Activity {
-	
-	private static boolean confirmMoves = true;
+public class SettingsActivity extends PreferenceActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_settings);
-		
-		SeekBar masterVolumeControl = (SeekBar) findViewById(R.id.masterVolumeControl);
-		SeekBar musicVolumeControl = (SeekBar) findViewById(R.id.musicVolumeControl);
-		SeekBar sfxVolumeControl = (SeekBar) findViewById(R.id.sfxVolumeControl);
-		
-		CheckBox confirmMoves = (CheckBox) findViewById(R.id.confirmMoves);
-		confirmMoves.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			public void onCheckedChanged(CompoundButton buttonView,
-                    boolean isChecked) {
-				setConfirmMoves(isChecked);
-			}
-		});
-		
-		CheckBox animations = (CheckBox) findViewById(R.id.animationsCheckBox);
-		
-		Button editPlayerButton = (Button) findViewById(R.id.editPlayerButton);
-		Button cancelButton = (Button) findViewById(R.id.cancelButton);
-		Button saveButton = (Button) findViewById(R.id.saveButton);
+        addPreferencesFromResource(R.xml.preferences);
+
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+		CheckBoxPreference confirmMoves = (CheckBoxPreference)findPreference("confirmMoves");
+		confirmMoves.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                SharedPreferences.Editor ed = prefs.edit();
+                return true;
+            }
+        });
+
 	}
-	
-	private void setConfirmMoves(boolean confirm) {
-		confirmMoves = confirm;
-	}
-	
-	public static boolean getConfirmMoves() {
-		return confirmMoves;
-	}
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            Preference Pref = findPreference(key);
+            Pref.setDefaultValue(sharedPreferences.getBoolean(key, ((CheckBoxPreference)findPreference(key)).isChecked()));
+    }
 }
